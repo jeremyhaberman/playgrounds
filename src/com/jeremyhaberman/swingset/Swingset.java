ef6e530865623ddec0141f24b7964b9396a7aad3
Jeremy Haberman <jeremy@jeremyhaberman.com>
2011-02-09 10:53:53 -0600

    Created Android project in Eclipse

---
+++ src/com/jeremyhaberman/swingset/Swingset.java
@@ -0,0 +1,13 @@
+package com.jeremyhaberman.swingset;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Swingset extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
